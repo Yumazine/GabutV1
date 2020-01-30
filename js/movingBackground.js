fb2a9918cc0afe0acdea2895381fe0ffd8a7f296
yumazine <yumazine@gmail.com>
2020-01-30 23:42:45 +0700

    asd

--- js/movingBackground.js
+++ js/movingBackground.js
@@ -3,31 +3,30 @@ function onload(){
 
     let movingBgIdx = 0
     movingBgArr[0].className += ' moving-bg-active'
-    movingBgArr[0].style = "transform: translate(-7em, -8em)"
-    movingBgArr[1].style = "transform: translate(-7em, -12em)"
-    movingBgArr[2].style = "transform: translate(-16em, 0)"
+    movingBgArr[0].style = "transform: translate(-10vw, -14vh)"
+    movingBgArr[1].style = "transform: translate(-7vw, -26vh)"
+    movingBgArr[2].style = "transform: translate(-21vw, 0)"
     
     let isFirst = true
 
     setInterval(function(){
         let i = movingBgIdx
         setTimeout(function(){
-            if(i % movingBgArr.length == 0 && !isFirst)
+            if(i == 0)
                 movingBgArr[0].style = "transform: translate(0, 0)"
-            else if(i % movingBgArr.length == 1)
-                movingBgArr[1].style = "transform: translate(-7em, -12em)"
+            else if(i == 1)
+                movingBgArr[1].style = "transform: translate(-7vw, -26vh)"
             else
-                movingBgArr[2].style = "transform: translate(-16em, 0)"
-            
-            isFirst = false
+                movingBgArr[2].style = "transform: translate(-21vw, 0)"
         }, 750)
-        movingBgArr[movingBgIdx++ % movingBgArr.length].className = 'moving-bg'
-        movingBgArr[movingBgIdx % movingBgArr.length].className += ' moving-bg-active'
-        if(movingBgIdx % movingBgArr.length == 0)
-            movingBgArr[0].style = "transform: translate(-7em, -8em)"
-        else if(movingBgIdx % movingBgArr.length == 1)
-            movingBgArr[1].style = "transform: translate(-2em, 0em)"
-        else if(movingBgIdx % movingBgArr.length == 2)
-            movingBgArr[2].style = "transform: translate(-16em, -15em)"
+        movingBgArr[movingBgIdx++].className = 'moving-bg'
+        movingBgIdx %= movingBgArr.length
+        movingBgArr[movingBgIdx].className += ' moving-bg-active'
+        if(movingBgIdx == 0)
+            movingBgArr[0].style = "transform: translate(-10vw, -14vh)"
+        else if(movingBgIdx == 1)
+            movingBgArr[1].style = "transform: translate(-2vw, 0)"
+        else if(movingBgIdx == 2)
+            movingBgArr[2].style = "transform: translate(-21vw, -40vh)"
     }, 8000)
 }
