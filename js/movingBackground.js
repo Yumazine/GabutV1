04acc39f3bbbbe6496e5521f2fd57b561e805b7c
yumazine <yumazine@gmail.com>
2020-01-31 14:46:42 +0700

    asd

--- js/movingBackground.js
+++ js/movingBackground.js
@@ -3,11 +3,11 @@ function onload(){
 
     let movingBgIdx = 0
     movingBgArr[0].className += ' moving-bg-active'
-    movingBgArr[0].style = "transform: translate(-10vw, -14vh)"
-    movingBgArr[1].style = "transform: translate(-7vw, -26vh)"
-    movingBgArr[2].style = "transform: translate(-21vw, 0)"
+    movingBgArr[0].style = "transform: translate(-" + (movingBgArr[0].width - window.innerWidth) + "px, -" + (movingBgArr[0].height - window.innerHeight) + "px)"
+    movingBgArr[1].style = "transform: translate(-" + (movingBgArr[1].width - window.innerWidth) + "px, -" + (movingBgArr[1].height - window.innerHeight) + "px)"
+    movingBgArr[2].style = "transform: translate(-" + (movingBgArr[2].width - window.innerWidth) + "px, 0)"
     
-    let isFirst = true
+    console.log(movingBgArr[0].width, window.innerWidth)
 
     setInterval(function(){
         let i = movingBgIdx
@@ -15,18 +15,18 @@ function onload(){
             if(i == 0)
                 movingBgArr[0].style = "transform: translate(0, 0)"
             else if(i == 1)
-                movingBgArr[1].style = "transform: translate(-7vw, -26vh)"
+                movingBgArr[1].style = "transform: translate(-" + (movingBgArr[1].width - window.innerWidth) + "px, -" + (movingBgArr[1].height - window.innerHeight) + "px)"
             else
-                movingBgArr[2].style = "transform: translate(-21vw, 0)"
+                movingBgArr[2].style = "transform: translate(-" + (movingBgArr[2].width - window.innerWidth) + "px, 0)"
         }, 750)
         movingBgArr[movingBgIdx++].className = 'moving-bg'
         movingBgIdx %= movingBgArr.length
         movingBgArr[movingBgIdx].className += ' moving-bg-active'
         if(movingBgIdx == 0)
-            movingBgArr[0].style = "transform: translate(-10vw, -14vh)"
+            movingBgArr[0].style = "transform: translate(-" + (movingBgArr[0].width - window.innerWidth) + "px, -" + (movingBgArr[0].height - window.innerHeight) + "px)"
         else if(movingBgIdx == 1)
-            movingBgArr[1].style = "transform: translate(-2vw, 0)"
+            movingBgArr[1].style = "transform: translate(0px, 0px)"
         else if(movingBgIdx == 2)
-            movingBgArr[2].style = "transform: translate(-21vw, -40vh)"
+            movingBgArr[2].style = "transform: translate(-" + (movingBgArr[2].width - window.innerWidth) + "px, -" + (movingBgArr[2].height - window.innerHeight) + "px)"
     }, 8000)
 }
