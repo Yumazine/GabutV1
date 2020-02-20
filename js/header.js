7932b44c7089d0825bb7bbc950936cd3df66395f
Christian Alvin <allfind99@gmail.com>
2020-02-20 23:39:47 +0700

    asd

---
+++ js/header.js
@@ -0,0 +1,9 @@
+const navMenu = document.getElementById('nav-menu')
+
+function navMenuClick(){
+    navMenu.style = "transform: translateX(0)"
+}
+
+function closeNavMenu(){
+    navMenu.style = ""
+}
