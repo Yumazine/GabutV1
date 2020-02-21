e617aefcd182e32a4096c8956556351fd14ebc37
yumazine <yumazine@gmail.com>
2020-02-21 10:35:18 +0700

    asd

--- js/header.js
+++ js/header.js
@@ -1,9 +1,12 @@
 const navMenu = document.getElementById('nav-menu')
+const navOuter = document.getElementById('nav-outer')
 
 function navMenuClick(){
     navMenu.style = "transform: translateX(0)"
+    navOuter.style = "opacity: .5"
 }
 
 function closeNavMenu(){
     navMenu.style = ""
+    navOuter.style = ""
 }
